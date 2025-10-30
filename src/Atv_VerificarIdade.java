fa7ac81bc1fbab29bf5a5397e6fa8d9a5aac669a
Lucas <0001152815@senaimgaluno.com.br>
2025-10-30 10:57:40 -0300

    Mensagem do commit

--- src/Atv_VerificarIdade.java
+++ src/Atv_VerificarIdade.java
@@ -2,13 +2,31 @@
 import java.util.Scanner;
 
 public class Atv_VerificarIdade {
-    public static void main(String[] args){
+    public static void main(String[] args) {
 
+        // Declarando a variável scanner
         Scanner sc = new Scanner(System.in);
 
+        // Obtendo idade
         System.out.println("Digite sua idade: ");
+            int idade = sc.nextInt();
 
-        for
+        // Verificando idade do usuário
+        if (idade <= 14) {
+            System.out.println("Você é criança.");
+        }
+
+        else if (idade < 18) {
+            System.out.println("Você é adolescente.");
+        }
+
+        else if (idade < 60) {
+            System.out.println("Você é adulto.");
+        }
+
+        else {
+            System.out.println("Você é idoso.");
+        }
 
         sc.close();
     }
