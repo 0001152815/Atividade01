50b176837c64fdb1e7e35a13bce9aa3efbec7c40
Lucas <0001152815@senaimgaluno.com.br>
2025-10-30 11:10:53 -0300

    Criando da temperatura

---
+++ src/Atv_Celsius.java
@@ -0,0 +1,14 @@
+// Importando biblioteca
+import java.util.Scanner;
+
+// Nota: Equações das temperaturas:
+// F=(C×1,8)+32.
+// (32∘F−32)×5/9=0∘C
+
+public class Atv_Celsius {
+    public static void main(String[] args) {
+
+        // Declarando variável scanner
+        Scanner sc = new Scanner(System.in);
+    }
+}
